1d0e7c098612a652e2acd58c74a9caa8fddd668e
chisaramai <eydel@hm.edu>
2016-10-27 20:59:34 +0200

    it somehow works

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,10 @@ import Control.Monad
 import Data.Char 
 import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
 
+
 main = do
-      hSetBuffering stdout NoBuffering
-      args <- getArgs :: IO [String]
-      let round = zip [1..] (nub (args !! 0))
-      let oldGrid = makeGrid (read (args !! 1) :: Int) (read (args !! 2) :: Int)
-      choice <- getChoice
-      putStrLn ("your choice: " ++ choice)
-      showGrid (dropToken 'O' (read choice :: Int) oldGrid)
-      main
+          hSetBuffering stdout NoBuffering
+          args <- getArgs :: IO [String]
+          let round = zip [1..] (nub (args !! 0))
+          let grid = makeGrid (read (args !! 1) :: Int) (read (args !! 2) :: Int)
+          playWith round grid
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( showGrid
     , getChoice
+    , dropToken
+    , playWith
     ) where
 
 import System.IO
@@ -12,14 +14,18 @@ import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
 showGrid :: [[Char]] -> IO()
 showGrid grid = do
       putStrLn (intercalate "\n" ((transpose grid) -- show dots for every position in the grid
-              ++ [filter isAlphaNum (unwords (map show [1..(length grid)]))]))  -- show column numbers
+              ++ [filter isAlphaNum (unwords (map show [1..(length grid)]))] ++ [replicate 16 '-']))  -- show column numbers
 
 --getChoice
 getChoice = do
             hSetBuffering stdout NoBuffering
             putStr "Choose column: "
-            choice <- getLine
-            return choice
+            choice <- getLine :: IO String -- no parse error
+            if ((choice /= []) && not (isDigit (head choice)) && (0 /= (length choice) ))
+                then
+                getChoice
+                else
+                return choice
 
 makeGrid :: Int -> Int -> [[Char]]
 makeGrid m n       =  replicate m (replicate n '.') -- ^ sets up a m * n grid, filled with points at each position
@@ -36,3 +42,14 @@ dropToken token columnNumber grid
                                                          newGrid           = (init (fst gridPieces)) ++ (changedColumn : []) ++ (snd gridPieces)
                                                      in newGrid
             | otherwise = grid
+            
+playWith :: [(Int,Char)] -> [[Char]] -> IO b
+playWith  round grid = do
+          choice <- getChoice
+          putStrLn ("your choice: " ++ choice)
+          let currentToken = head round
+              changedGrid = (dropToken (snd currentToken) (read choice :: Int) grid)
+              changedRound = (tail round) ++ (currentToken:[])
+          showGrid changedGrid
+          playWith changedRound changedGrid
+  
