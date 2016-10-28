1fec9bfecf239297c11ba6a774328a5fb800bf1e
chisaramai <eydel@hm.edu>
2016-10-28 17:24:36 +0200

    some more works

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,35 +17,34 @@ showGrid grid = do
               ++ [filter isAlphaNum (unwords (map show [1..(length grid)]))] ++ [replicate 16 '-']))  -- show column numbers
 
 --getChoice
-getChoice = do
+getChoice from to = do
             hSetBuffering stdout NoBuffering
             putStr "Choose column: "
             choice <- getLine :: IO String -- no parse error
-            if ((choice /= []) && not (isDigit (head choice)) && (0 /= (length choice) ))
+            if ((not(null choice)) && (isDigit (head choice)) && (from <= (read choice :: Int)) && (read choice :: Int) <= to)
                 then
-                getChoice
-                else
                 return choice
+                else
+                getChoice from to
 
 makeGrid :: Int -> Int -> [[Char]]
 makeGrid m n       =  replicate m (replicate n '.') -- ^ sets up a m * n grid, filled with points at each position
                        
 
 dropToken :: Char -> Int -> [[Char]] -> [[Char]]
-dropToken token columnNumber grid 
-            | columnNumber <= (length (head grid)) = let columnIndex       = columnNumber - 1 -- ^ f.e. 1234 -> 0123
-                                                         originalColumn    = grid !! columnIndex -- ^ extract the chosen column
-                                                         numberOfEmptyRows = filter (== '.') originalColumn -- ^ scan how many free rows are left in that column
-                                                         columnPieces      = splitAt (length numberOfEmptyRows) originalColumn -- ^ so it will look like this: (["..."],["XO"])
-                                                         changedColumn     = (init (fst columnPieces)) ++ (token : []) ++ (snd columnPieces) -- ^ insert the token at the end of rhe first List
-                                                         gridPieces        = splitAt columnNumber grid
-                                                         newGrid           = (init (fst gridPieces)) ++ (changedColumn : []) ++ (snd gridPieces)
-                                                     in newGrid
-            | otherwise = grid
+dropToken token columnNumber grid =
+                                         let columnIndex  = columnNumber - 1 -- ^ f.e. 1234 -> 0123
+                                             originalColumn    = grid !! columnIndex -- ^ extract the chosen column
+                                             emptyRows         = filter (== '.') originalColumn -- ^ scan how many free rows are left in that column
+                                             columnPieces      = splitAt (length emptyRows) originalColumn -- ^ so it will look like this: (["..."],["XO"])
+                                             changedColumn     = (init (fst columnPieces)) ++ (token : []) ++ (snd columnPieces) -- ^ insert the token at the end of rhe first List
+                                             gridPieces        = splitAt columnNumber grid
+                                             newGrid           = (init (fst gridPieces)) ++ (changedColumn : []) ++ (snd gridPieces)
+                                         in newGrid
             
 playWith :: [(Int,Char)] -> [[Char]] -> IO b
 playWith  round grid = do
-          choice <- getChoice
+          choice <- getChoice 1 (length grid)
           putStrLn ("your choice: " ++ choice)
           let currentToken = head round
               changedGrid = (dropToken (snd currentToken) (read choice :: Int) grid)
